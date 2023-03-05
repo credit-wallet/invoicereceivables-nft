7d1a051785075c3982e2844319d8a0caee8a0ff7
mnm458 <mohaknainmalhotra@gmail.com>
2023-03-05 05:56:30 -0700

    invoice nft

--- deploy/deploy-factory.ts
+++ deploy/deploy-factory.ts
@@ -5,26 +5,21 @@ import { Deployer } from '@matterlabs/hardhat-zksync-deploy';
 
 export default async function (hre: HardhatRuntimeEnvironment) {
   console.log("running");
-  const wallet = new Wallet('wallet');
+  const wallet = new Wallet('0760f12e22b81d72ab9cbf7321990b9c05a73cf5bf0f0e180b9382eb9abc9fc8');
   const deployer = new Deployer(hre, wallet);
-  const factoryArtifact = await deployer.loadArtifact('AAFactory');
-  const aaArtifact = await deployer.loadArtifact('TwoUserMultisig');
+  const invoiceNFTArtifact = await deployer.loadArtifact('InvoiceNFT');
+  // const aaArtifact = await deployer.loadArtifact('TwoUserMultisig');
   console.log("1");
 
+  const usdcAddr: string[] = ["0x07865c6e87b9f70255377e024ace6630c1eaa37f"];
 
   // Getting the bytecodeHash of the account
-  const bytecodeHash = utils.hashBytecode(aaArtifact.bytecode);
+  // const bytecodeHash = utils.hashBytecode(aaArtifact.bytecode);
 
   const factory = await deployer.deploy(
-    factoryArtifact,
-    [bytecodeHash],
-    undefined,
-    [
-      // Since the factory requires the code of the multisig to be available,
-      // we should pass it here as well.
-      aaArtifact.bytecode,
-    ],
+    invoiceNFTArtifact,
+    usdcAddr,
   );
 
-  console.log(`AA factory address: ${factory.address}`);
+  console.log(`Invoice NFT address: ${factory.address}`);
 }
